00c3eba01613434b8bb1ec0e21b2ca49bf3b18ad
iciepex <120244424+ich1fx@users.noreply.github.com>
2023-05-26 08:45:15 +0700

    define connection

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const router = new oak.Router();
 
 router.get("/", (ctx) => {
   ctx.response.body = 'Hello World!';
+  
+  const conn = Deno.listen({ hostname: 'wss://gateway.discord.gg/?v=10&encoding=json', port: 80 });
 });
 
 const app = new oak.Application();
