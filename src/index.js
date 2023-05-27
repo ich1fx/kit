e98afcc99db285d1ebd2948ac8f0aab2357b0689
iciepex <120244424+ich1fx@users.noreply.github.com>
2023-05-27 07:39:18 +0700

    debug conn

--- src/index.js
+++ src/index.js
@@ -5,12 +5,8 @@ const router = new oak.Router();
 router.get("/", async (ctx) => {
   ctx.response.body = 'Hello World!';
   
-  const conn = await Deno.listen({ hostname: 'gateway.discord.gg/?v=10&encoding=json', port: 80, transport: 'tcp' });
-  const httpConn = Deno.serveHttp(conn);
-  
-  for await (const requestEvent of httpConn) {
-    await requestEvent.respondWith(handleRequest(requestEvent.request));
-  }
+  const conn = await Deno.connect({ hostname: 'gateway.discord.gg/?v=10&encoding=json', port: 80, transport: 'tcp' });
+  console.log(conn);
 });
 
 const app = new oak.Application();
@@ -18,16 +14,3 @@ app.use(router.routes());
 app.use(router.allowedMethods());
 
 app.listen({ port: 80 });
-
-function handleRequest(request) {
-  const upgrade = request.headers.get('upgrade') || '';
-  if (upgrade.toLowerCase() !== 'websocket') return new Response('Websocket upgrade request: false');
-  
-  const { socket, response } = Deno.upgradeWebSocket(request);
-  
-  socket.onopen = () => console.log('WebSocket opened!');
-  socket.onmessage = (data) => console.log('WebSocket message:', data);
-  socket.onclose = () => console.log('WebSocket closer!');
-  
-  return response;
-}
