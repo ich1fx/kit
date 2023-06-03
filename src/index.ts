683ad42eda226df61e03f01fce26156312a63df3
iciepex <120244424+ich1fx@users.noreply.github.com>
2023-06-03 11:30:29 +0700

    fetch dlx body

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,9 @@ import { oak } from './deps.ts';
 const router = new oak.Router();
 
 router.get("/", async (ctx) => {
-  ctx.response.body = 'Hello World!';
-  console.log(ctx.cookies);
-  console.log(ctx.request.ip);
-  console.log(ctx.request.ips);
-  
-  for (const [k, v] of ctx.request.url.searchParams.entries()) {
-    console.log(k, v);
-  }
+  const denolandX = await fetch("https://deno.land/x");
+  const dlx = await response.body();
+  ctx.response.body = dlx;
 });
 
 const app = new oak.Application({
