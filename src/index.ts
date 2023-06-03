622d06fecbab8ef2fccaed5d267998c810f7aa15
iciepex <120244424+ich1fx@users.noreply.github.com>
2023-06-04 04:40:16 +0700

    for of method test

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,12 @@ router.get("/", async (ctx) => {
     .filter(file => file.name.endsWith('.ts'));
   Promise.all(dir.map(file => import(`./${file.name}`)))
     .then(console.log);
+  
+  console.log('for of method');
+  for (const file of dir) {
+    const route = await import(`./$(file.name}`);
+    console.log(route);
+  }
 });
 
 const app = new oak.Application({
