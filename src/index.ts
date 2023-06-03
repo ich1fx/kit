f5e31a86569689af2dbb1e4ad035552e723d9a51
iciepex <120244424+ich1fx@users.noreply.github.com>
2023-06-04 04:43:40 +0700

    move context area

--- src/index.ts
+++ src/index.ts
@@ -4,18 +4,12 @@ const router = new oak.Router();
 
 router.get("/", async (ctx) => {
   ctx.response.body = "Hello!";
-  
+});
+
   const dir = Array.from(Deno.readDirSync(Deno.cwd() + '/src'))
     .filter(file => file.name.endsWith('.ts'));
   Promise.all(dir.map(file => import(`./${file.name}`)))
     .then(console.log);
-  
-  console.log('for of method');
-  for (const file of dir) {
-    const route = await import(`./$(file.name}`);
-    console.log(route);
-  }
-});
 
 const app = new oak.Application({
   proxy: true 
