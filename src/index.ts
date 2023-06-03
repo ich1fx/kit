37a8de2346dd70917f008155fe856ecdb932c6aa
iciepex <120244424+ich1fx@users.noreply.github.com>
2023-06-03 08:03:56 +0700

    define application proxy

--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,17 @@ const router = new oak.Router();
 router.get("/", async (ctx) => {
   ctx.response.body = 'Hello World!';
   console.log(ctx.cookies);
-  console.log(ctx.request.headers);
+  console.log(ctx.request.ip);
+  console.log(ctx.request.ips);
   
   for (const [k, v] of ctx.request.url.searchParams.entries()) {
     console.log(k, v);
   }
 });
 
-const app = new oak.Application();
+const app = new oak.Application({
+  proxy: true 
+});
 app.use(router.routes());
 app.use(router.allowedMethods());
 
